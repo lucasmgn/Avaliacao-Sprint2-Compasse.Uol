cc07cdf6539f629693dc37d89e58636a57b35488
Lucas <lucasmagno695@gmail.com>
2022-10-26 19:29:31 -0300

    Adicionado classes iniciais

---
+++ src/questao1/model/Produto.java
@@ -0,0 +1,35 @@
+package questao1.model;
+
+import java.math.BigDecimal;
+
+public class Produto {
+
+    private int id;
+    private String nome;
+    private String descricao;
+    private int quantidade;
+    private BigDecimal preco;
+
+    public Produto(String nome, String descricao, int quantidade, BigDecimal preco) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+}
